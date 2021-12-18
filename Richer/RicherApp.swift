2a8e7599a3c3c6942e9e71f94e7e55dadaf07a92
lex-yang <lex.yang@wadamake.com>
2021-12-19 02:56:53 +0800

    Initial Commit

---
+++ Richer/RicherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicherApp.swift
+//  Richer
+//
+//  Created by Lex Yang on 2021/12/19.
+//
+
+import SwiftUI
+
+@main
+struct RicherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
